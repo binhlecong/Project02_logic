2e95fea25e5bce6b67b832cc322c76dd2bc0d63c
binhlecong <congbinh21012001@gmail.com>
2021-12-04 14:11:06 +0700

    update - easier to test test case

--- PS4/SRC/main.py
+++ PS4/SRC/main.py
@@ -6,18 +6,26 @@ from const import *
 def main():
     '''Main function'''
     # Read from input.txt
+    alpha = 'A'
     problem = [AND,
-               [OR, [NOT, 'A'], 'B'],
-               [OR, 'B', [NOT, 'C']],
-               [OR, 'A', [NOT, 'B'], 'C'],
-               [NOT, 'B'],
+               'A',
+               [OR, 'B', 'C', [NOT, 'B'], [NOT, 'C']],
+               [OR, 'X', 'Y', 'Z'],
+               [NOT, 'A'],
+               ]
+
+    alpha = [OR, 'B', [NOT, 'B']]
+    problem = [AND,
+               'A',
+               [OR, 'B', 'C', [NOT, 'B'], [NOT, 'C']],
+               [OR, 'X', 'Y', 'Z'],
+               [NOT, 'A'],
                ]
 
     kb = KnowledgeBase()
     kb.detailsTurn = True
     for cl in problem[1:]:
         kb.tell(cl)
-    alpha = 'A'
 
     # Call PL-Resolution fucntion
     output, result = plResolution(kb, alpha)
