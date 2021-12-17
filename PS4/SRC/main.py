b055eb05875b11e3bde0e45f116a70f231b3ac43
binhlecong <congbinh21012001@gmail.com>
2021-12-18 02:05:11 +0700

    feat: 6th input file

--- PS4/SRC/main.py
+++ PS4/SRC/main.py
@@ -7,7 +7,7 @@ from const import *
 def main():
     '''Main function'''
     start = 1
-    end = 5
+    end = 6
     for i in range(start, end + 1):
         # Read from input_.txt
         alpha, kb = read_file('input/input{}.txt'.format(str(i)))
